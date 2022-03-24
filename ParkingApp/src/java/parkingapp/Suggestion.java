35760edd1df0191f03ce6bb74042e8292f371c70
lukelego <98852588+lukelego@users.noreply.github.com>
2022-03-24 11:38:09 -0500

    Update Suggestion.java
    Implemented Suggestion

--- ParkingApp/src/java/parkingapp/Suggestion.java
+++ ParkingApp/src/java/parkingapp/Suggestion.java
@@ -1,13 +1,53 @@
+
 package parkingapp;
 
-// @author Sam Hildebrand
-public class Suggestion {
-    private String buildingName;
+import java.util.*;
+
+/**
+ *
+ * @author Sam and Luke LeGoullon
+ */
+public class Suggestion implements Comparable<Suggestion>{    
     
-    public String toArray(){
-        return "found";
+    private double score;
+    private ParkingLot lot;
+    private String buildingName;
+    private double totalParkTime;
+    
+    
+    public Suggestion(ParkingLot parkLot, String nameOfBuilding){
+        lot = parkLot;
+        buildingName = nameOfBuilding;
+        totalParkTime = Lots_Calc.CalculateTime(lot, buildingName); //Static method from the lot calculator class
+        score = Lots_Calc.CalculateScore(lot, buildingName, totalParkTime);
     }
-    public Suggestion(String name){
-        buildingName = name;
+    
+    public double getScore(){
+        return score;
     }
+    
+    public ParkingLot getLot(){
+        return lot;
+    }
+    
+    public String getBuildingName(){
+        return buildingName;
+    }
+    
+    public double getParkingtime(){
+        return totalParkTime;
+    }
+    
+    @Override
+    public int compareTo(Suggestion other){
+        if(this.score - other.score >= 0){
+            return 1;
+        } else {
+            return -1;
+        }
+    }
+    
+    
+    
 }
+

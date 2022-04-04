b81fad53c1c0c2d76064f3e773fa681f05beaa80
lukelego <98852588+lukelego@users.noreply.github.com>
2022-04-03 20:35:12 -0500

    Update ParkingLot.java
    Add comparable to parkingLot

--- ParkingApp/src/java/parkingappcalculations/ParkingLot.java
+++ ParkingApp/src/java/parkingappcalculations/ParkingLot.java
@@ -1,12 +1,15 @@
 package parkingappcalculations;
 
+import java.util.*;
 
-public class ParkingLot {
+public class ParkingLot implements Comparable<ParkingLot> {
    
     private String name;
     private int capacity;
     private boolean isFavorite; //stretch feature
     private double averageRating;
+    private ArrayList<Rating> lotRatingList;
+
     
     //Does each parking lot have an arrayList of Lot ratings at that time?
     public ParkingLot(String lotName, double lotPercentFull, int lotCapacity) {
@@ -39,8 +42,12 @@ public class ParkingLot {
     }
     
     public void addLotRating(double rating){
-        
+        lotRatingList.add(rating);
     }
     
+    @Override
+    public int compareTo(ParkingLot other){
+        return this.getName().compareToIgnoreCase(other.getName());
+    }
 
 }

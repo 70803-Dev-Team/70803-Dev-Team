f58b5317c572fbd94f89c00139f756715c6f08ec
Sam Hildebrand <sch_numberone@yahoo.com>
2022-04-06 10:52:03 -0500

    fixes

--- ParkingApp/src/java/parkingappWeblets/LotSugestions.java
+++ ParkingApp/src/java/parkingappWeblets/LotSugestions.java
@@ -39,93 +39,97 @@ public class LotSugestions extends HttpServlet {
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         try ( PrintWriter out = response.getWriter()) {
-            DataBase db = DataBase.getInstance();
-            Cookie cookies[] = request.getCookies(); 
-            User currentUser = new User(cookies[0].getValue(), cookies[1].getValue());
-            User dbUser = db.getUser(cookies[0].getValue());
-            if(!dbUser.compareTo(currentUser)){
-               response.sendRedirect("index.html"); 
-            } else {
-                String buildingName = request.getParameter("buildingName");
-                out.println("<!DOCTYPE html>\n" +
-                "<html>\n" +
-                "<head>\n" +
-                "    <meta name=\"viewport\" content=\"width=device-width, initial scale=1.0\">\n" +
-                "    <title>ParkingApp</title>\n" +
-                "    <link rel=\"stylesheet\" href=\"style_map.css\">\n" +
-                "    <link rel=\"preconnect\" href=\"https://fonts.googleapis.com\">\n" +
-                "    <link rel=\"preconnect\" href=\"https://fonts.gstatic.com\" crossorigin>\n" +
-                "    <link href=\"https://fonts.googleapis.com/css2?family=Raleway:wght@100;200;300;400;600;700&display=swap\" \n" +
-                "    rel=\"stylesheet\">\n" +
-                "    <link rel=\"stylesheet\" href=\"https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css\">\n" +
-                "    <script src='https://api.mapbox.com/mapbox-gl-js/v2.3.1/mapbox-gl.js'></script>\n" +
-                "    <link href='https://api.mapbox.com/mapbox-gl-js/v2.3.1/mapbox-gl.css' rel='stylesheet' />     \n" +
-                "    <script src=\"map.js\" defer></script>\n" +
-                "</head>\n" +
-                "    <body>\n" +
-                "        <div class=\"main_box\"></div>\n" +
-                "        <header tabindex=\"0\">LSU Parking Assistant</header>\n" +
-                "\n" +
-                "        <!-----Side Slide-out Menu----->\n" +
-                "        <div id=\"nav-container\">\n" +
-                "            <div class=\"bg\"></div>\n" +
-                "            <div class=\"button\" tabindex=\"0\">\n" +
-                "              <span class=\"icon-bar\"></span>\n" +
-                "              <span class=\"icon-bar\"></span>\n" +
-                "              <span class=\"icon-bar\"></span>\n" +
-                "            </div>\n" +
-                "            <div id=\"nav-content\" tabindex=\"0\">\n" +
-                "              <ul>\n" +
-                "                <li><a href=\"#\">General Settings</a></li>\n" +
-                "                <li><a href=\"#\">Account Settings</a></li>\n" +
-                "                <li><a href=\"#\">Help Center</a></li>\n" +
-                "                <li><a href=\"https://github.com/70803-Dev-Team/70803-Dev-Team/issues\\\">Report an issue</a></li>\n" +
-                "                <li><a href=\"#\">Log out</a></li>\n" +
-                "                <li class=\"small\">\n" +
-                "                    <i class=\"fa fa-copyright\"></i>2022 70803 Dev Team, Inc. All Rights Reserved</a>\n" +
-                "                </li>\n" +
-                "              </ul>\n" +
-                "            </div>\n" +
-                "          </div>\n" +
-                "\n" +
-                "        <!-----Directions Box----->\n" +
-                "        <div id=\"directionsOverlay\">\n" +
-                "            <h1>Results</h1>\n" +
-                "                <div class=\"parkingLotList\">");
-                SuggestionCompilation sg = new SuggestionCompilation(buildingName, "Commuter");
-                ArrayList<Suggestion> suggestions = sg.getSuggestions();
-                for (int i = 0; i < 5; i++){
-                    Suggestion suggestion = suggestions.get(i);
-                    String suggestionName = suggestion.getLot().getName();
-                    out.println("<h2>"+ suggestionName +"</h2>\n");
-                    int rating = db.getRating(suggestionName);
-                    if (rating == -9999){
-                        out.println("Rating Unavailable");
-                    }else{
-                        out.println(rating*20+"% full");
-                    }
-                    out.println(" | ");
-                    double distance = Graph.MinDistance(buildingName, suggestion.getLot(), "Commuter");
-                    if (distance == -9999){
-                        out.println("Distance Unavailable"); 
-                    } else if (distance > 5280) {
-                        distance = distance/5280;
-                        out.printf("%.1fmi. away", distance); 
-                    } else {
-                        out.printf("%.0fft away", distance);
-                    }
-                    out.println("<li><a href=\"RateLot?LotName=" + suggestionName + "\">Rate Lot</a></li>");
-                    out.println("<br>");
+            try {
+                DataBase db = DataBase.getInstance();
+                Cookie cookies[] = request.getCookies(); 
+                User currentUser = new User(cookies[0].getValue(), cookies[1].getValue());
+                User dbUser = db.getUser(cookies[0].getValue());
+                if(!dbUser.compareTo(currentUser)){
+                   response.sendRedirect("index.html"); 
+                } else {
+                    String buildingName = request.getParameter("buildingName");
+                    out.println("<!DOCTYPE html>\n" +
+                    "<html>\n" +
+                    "<head>\n" +
+                    "    <meta name=\"viewport\" content=\"width=device-width, initial scale=1.0\">\n" +
+                    "    <title>ParkingApp</title>\n" +
+                    "    <link rel=\"stylesheet\" href=\"style_map.css\">\n" +
+                    "    <link rel=\"preconnect\" href=\"https://fonts.googleapis.com\">\n" +
+                    "    <link rel=\"preconnect\" href=\"https://fonts.gstatic.com\" crossorigin>\n" +
+                    "    <link href=\"https://fonts.googleapis.com/css2?family=Raleway:wght@100;200;300;400;600;700&display=swap\" \n" +
+                    "    rel=\"stylesheet\">\n" +
+                    "    <link rel=\"stylesheet\" href=\"https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css\">\n" +
+                    "    <script src='https://api.mapbox.com/mapbox-gl-js/v2.3.1/mapbox-gl.js'></script>\n" +
+                    "    <link href='https://api.mapbox.com/mapbox-gl-js/v2.3.1/mapbox-gl.css' rel='stylesheet' />     \n" +
+                    "    <script src=\"map.js\" defer></script>\n" +
+                    "</head>\n" +
+                    "    <body>\n" +
+                    "        <div class=\"main_box\"></div>\n" +
+                    "        <header tabindex=\"0\">LSU Parking Assistant</header>\n" +
+                    "\n" +
+                    "        <!-----Side Slide-out Menu----->\n" +
+                    "        <div id=\"nav-container\">\n" +
+                    "            <div class=\"bg\"></div>\n" +
+                    "            <div class=\"button\" tabindex=\"0\">\n" +
+                    "              <span class=\"icon-bar\"></span>\n" +
+                    "              <span class=\"icon-bar\"></span>\n" +
+                    "              <span class=\"icon-bar\"></span>\n" +
+                    "            </div>\n" +
+                    "            <div id=\"nav-content\" tabindex=\"0\">\n" +
+                    "              <ul>\n" +
+                    "                <li><a href=\"#\">General Settings</a></li>\n" +
+                    "                <li><a href=\"#\">Account Settings</a></li>\n" +
+                    "                <li><a href=\"#\">Help Center</a></li>\n" +
+                    "                <li><a href=\"https://github.com/70803-Dev-Team/70803-Dev-Team/issues\\\">Report an issue</a></li>\n" +
+                    "                <li><a href=\"#\">Log out</a></li>\n" +
+                    "                <li class=\"small\">\n" +
+                    "                    <i class=\"fa fa-copyright\"></i>2022 70803 Dev Team, Inc. All Rights Reserved</a>\n" +
+                    "                </li>\n" +
+                    "              </ul>\n" +
+                    "            </div>\n" +
+                    "          </div>\n" +
+                    "\n" +
+                    "        <!-----Directions Box----->\n" +
+                    "        <div id=\"directionsOverlay\">\n" +
+                    "            <h1>Results</h1>\n" +
+                    "                <div class=\"parkingLotList\">");
+                    SuggestionCompilation sg = new SuggestionCompilation(buildingName, "Commuter");
+                    ArrayList<Suggestion> suggestions = sg.getSuggestions();
+                    for (int i = 0; i < 5; i++){
+                        Suggestion suggestion = suggestions.get(i);
+                        String suggestionName = suggestion.getLot().getName();
+                        out.println("<h2>"+ suggestionName +"</h2>\n");
+                        int rating = db.getRating(suggestionName);
+                        if (rating == -9999){
+                            out.println("Rating Unavailable");
+                        }else{
+                            out.println(rating*20+"% full");
+                        }
+                        out.println(" | ");
+                        double distance = Graph.MinDistance(buildingName, suggestion.getLot(), "Commuter");
+                        if (distance == -9999){
+                            out.println("Distance Unavailable"); 
+                        } else if (distance > 5280) {
+                            distance = distance/5280;
+                            out.printf("%.1fmi. away", distance); 
+                        } else {
+                            out.printf("%.0fft away", distance);
+                        }
+                        out.println("<li><a href=\"RateLot?LotName=" + suggestionName + "\">Rate Lot</a></li>");
+                        out.println("<br>");
 
+                    }
+                    out.println("</div>\n" +
+                    "            <p class=\"rateLot\">\n" +
+                    "                <a href=\"ParkingApp\">Find a Parking Lot!</a>\n" +
+                    "            </p>\n" +
+                    "        </div>  \n" +
+                    "        <div id='map'></div>\n" +
+                    "    </body>\n" +
+                    "</html>\n");
                 }
-                out.println("</div>\n" +
-                "            <p class=\"rateLot\">\n" +
-                "                <a href=\"ParkingApp\">Find a Parking Lot!</a>\n" +
-                "            </p>\n" +
-                "        </div>  \n" +
-                "        <div id='map'></div>\n" +
-                "    </body>\n" +
-                "</html>\n");
+            } catch (Exception e){
+                response.sendRedirect("index.html");
             }
         }
     }
--- ParkingApp/src/java/parkingappWeblets/ParkingApp.java
+++ ParkingApp/src/java/parkingappWeblets/ParkingApp.java
@@ -31,81 +31,85 @@ public class ParkingApp extends HttpServlet {
             // Anytime we have a webapp java file like this one, we need to check
             // that the user is actually valid. So we check the cookies and test
             // the user against the one in the database like so.
-            Cookie cookies[] = request.getCookies();
-            DataBase db = DataBase.getInstance();
-            User currentUser = new User(cookies[0].getValue(), cookies[1].getValue());
-            User dbUser = db.getUser(cookies[0].getValue());
-            if(!dbUser.compareTo(currentUser)){
-                   response.sendRedirect("index.html"); 
-                } else {
-                    out.println("<!DOCTYPE html>\n" +
-                    "<html>\n" +
-                    "<head>\n" +
-                    "    <meta name=\"viewport\" content=\"width=device-width, initial scale=1.0\">\n" +
-                    "    <title>ParkingApp</title>\n" +
-                    "    <link rel=\"stylesheet\" href=\"style_map.css\">\n" +
-                    "    <link rel=\"preconnect\" href=\"https://fonts.googleapis.com\">\n" +
-                    "    <link rel=\"preconnect\" href=\"https://fonts.gstatic.com\" crossorigin>\n" +
-                    "    <link href=\"https://fonts.googleapis.com/css2?family=Raleway:wght@100;200;300;400;600;700&display=swap\" \n" +
-                    "    rel=\"stylesheet\">\n" +
-                    "    <link rel=\"stylesheet\" href=\"https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css\">\n" +
-                    "    <script src=\"https://kit.fontawesome.com/5d23708ef4.js\" crossorigin=\"anonymous\"></script>\n" +
-                    "    <script src='https://api.mapbox.com/mapbox-gl-js/v2.3.1/mapbox-gl.js'></script>\n" +
-                    "    <link href='https://api.mapbox.com/mapbox-gl-js/v2.3.1/mapbox-gl.css' rel='stylesheet' />     \n" +
-                    "    <script src=\"map.js\" defer></script>\n" +
-                    "    <script src=\"autocomplete-building.js\" defer></script>\n" +
-                    "</head>\n" +
-                    "    <body>\n" +
-                    "        <div class=\"main_box\"></div>\n" +
-                    "        <header tabindex=\"0\">LSU Parking Assistant</header>\n" +
-                    "\n" +
-                    "        <!-----Side Slide-out Menu----->\n" +
-                    "        <div id=\"nav-container\">\n" +
-                    "            <div class=\"bg\"></div>\n" +
-                    "            <div class=\"button\" tabindex=\"0\">\n" +
-                    "              <span class=\"icon-bar\"></span>\n" +
-                    "              <span class=\"icon-bar\"></span>\n" +
-                    "              <span class=\"icon-bar\"></span>\n" +
-                    "            </div>\n" +
-                    "            <div id=\"nav-content\" tabindex=\"0\">\n" +
-                    "              <ul>\n" +
-                    "                <li><a href=\"#\">General Settings</a></li>\n" +
-                    "                <li><a href=\"#\">Account Settings</a></li>\n" +
-                    "                <li><a href=\"#\">Help Center</a></li>\n" +
-                    "                <li><a href=\"https://github.com/70803-Dev-Team/70803-Dev-Team/issues\\\">Report an issue</a></li>\n" +
-                    "                <li><a href=\"index.html\">Log out</a></li>\n" +
-                    "                <li class=\"small\">\n" +
-                    "                    <i class=\"fa fa-copyright\"></i>2022 70803 Dev Team, Inc. All Rights Reserved</a>\n" +
-                    "                </li>\n" +
-                    "              </ul>\n" +
-                    "            </div>\n" +
-                    "          </div>\n" +
-                    "\n" +
-                    "        <!-----Directions Box----->\n" +
-                    "        <div id=\"directionsOverlay\">\n" +
-                    "            <h1>Find a Lot</h1>\n" +
-                    "            <form action=\"LotSugestions\" method=\"get\">\n" +
-                    "                <p class=\"form_group\">\n" +
-                    "                    <input id=\"myInput\" type=\"text\" class=\"form_field\" placeholder=\"Building you want to go to\" name=\"buildingName\">\n" +
-                    "                    <label for=\"name\" class=\"form_label\">Building you want to go to</label>\n" +
-                    "                </p>\n" +
-                    "            <p class=\"field\">\n" +
-                    "                <input type=\"submit\" value=\"Go\">\n" +
-                    "            </p>\n" +
-                    "            </form>\n" +
-                    "            <div class=\"other-info\">\n" +
-                    "                <p class=\"listLot\">\n" +
-                    "                    <a href=\"ListLot.html\">All Parking Lots!</a>\n" +
-                    "                </p>\n" +
-                    "                <p class=\"rateLot\">\n" +
-                    "                    <a href=\"RateLot\">Rate a parking spot!</a>\n" +
-                    "                </p>\n" +
-                    "            </div>\n" +
-                    "        </div>  \n" +
-                    "        <div id='map'></div>\n" +
-                    "    </body>\n" +
-                    "</html>\n" +
-                    "");
+            try {
+                Cookie cookies[] = request.getCookies();
+                DataBase db = DataBase.getInstance();
+                User currentUser = new User(cookies[0].getValue(), cookies[1].getValue());
+                User dbUser = db.getUser(cookies[0].getValue());
+                if(!dbUser.compareTo(currentUser)){
+                       response.sendRedirect("index.html"); 
+                    } else {
+                        out.println("<!DOCTYPE html>\n" +
+                        "<html>\n" +
+                        "<head>\n" +
+                        "    <meta name=\"viewport\" content=\"width=device-width, initial scale=1.0\">\n" +
+                        "    <title>ParkingApp</title>\n" +
+                        "    <link rel=\"stylesheet\" href=\"style_map.css\">\n" +
+                        "    <link rel=\"preconnect\" href=\"https://fonts.googleapis.com\">\n" +
+                        "    <link rel=\"preconnect\" href=\"https://fonts.gstatic.com\" crossorigin>\n" +
+                        "    <link href=\"https://fonts.googleapis.com/css2?family=Raleway:wght@100;200;300;400;600;700&display=swap\" \n" +
+                        "    rel=\"stylesheet\">\n" +
+                        "    <link rel=\"stylesheet\" href=\"https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css\">\n" +
+                        "    <script src=\"https://kit.fontawesome.com/5d23708ef4.js\" crossorigin=\"anonymous\"></script>\n" +
+                        "    <script src='https://api.mapbox.com/mapbox-gl-js/v2.3.1/mapbox-gl.js'></script>\n" +
+                        "    <link href='https://api.mapbox.com/mapbox-gl-js/v2.3.1/mapbox-gl.css' rel='stylesheet' />     \n" +
+                        "    <script src=\"map.js\" defer></script>\n" +
+                        "    <script src=\"autocomplete-building.js\" defer></script>\n" +
+                        "</head>\n" +
+                        "    <body>\n" +
+                        "        <div class=\"main_box\"></div>\n" +
+                        "        <header tabindex=\"0\">LSU Parking Assistant</header>\n" +
+                        "\n" +
+                        "        <!-----Side Slide-out Menu----->\n" +
+                        "        <div id=\"nav-container\">\n" +
+                        "            <div class=\"bg\"></div>\n" +
+                        "            <div class=\"button\" tabindex=\"0\">\n" +
+                        "              <span class=\"icon-bar\"></span>\n" +
+                        "              <span class=\"icon-bar\"></span>\n" +
+                        "              <span class=\"icon-bar\"></span>\n" +
+                        "            </div>\n" +
+                        "            <div id=\"nav-content\" tabindex=\"0\">\n" +
+                        "              <ul>\n" +
+                        "                <li><a href=\"#\">General Settings</a></li>\n" +
+                        "                <li><a href=\"#\">Account Settings</a></li>\n" +
+                        "                <li><a href=\"#\">Help Center</a></li>\n" +
+                        "                <li><a href=\"https://github.com/70803-Dev-Team/70803-Dev-Team/issues\\\">Report an issue</a></li>\n" +
+                        "                <li><a href=\"index.html\">Log out</a></li>\n" +
+                        "                <li class=\"small\">\n" +
+                        "                    <i class=\"fa fa-copyright\"></i>2022 70803 Dev Team, Inc. All Rights Reserved</a>\n" +
+                        "                </li>\n" +
+                        "              </ul>\n" +
+                        "            </div>\n" +
+                        "          </div>\n" +
+                        "\n" +
+                        "        <!-----Directions Box----->\n" +
+                        "        <div id=\"directionsOverlay\">\n" +
+                        "            <h1>Find a Lot</h1>\n" +
+                        "            <form action=\"LotSugestions\" method=\"get\">\n" +
+                        "                <p class=\"form_group\">\n" +
+                        "                    <input id=\"myInput\" type=\"text\" class=\"form_field\" placeholder=\"Building you want to go to\" name=\"buildingName\">\n" +
+                        "                    <label for=\"name\" class=\"form_label\">Building you want to go to</label>\n" +
+                        "                </p>\n" +
+                        "            <p class=\"field\">\n" +
+                        "                <input type=\"submit\" value=\"Go\">\n" +
+                        "            </p>\n" +
+                        "            </form>\n" +
+                        "            <div class=\"other-info\">\n" +
+                        "                <p class=\"listLot\">\n" +
+                        "                    <a href=\"ListLot.html\">All Parking Lots!</a>\n" +
+                        "                </p>\n" +
+                        "                <p class=\"rateLot\">\n" +
+                        "                    <a href=\"RateLot\">Rate a parking spot!</a>\n" +
+                        "                </p>\n" +
+                        "            </div>\n" +
+                        "        </div>  \n" +
+                        "        <div id='map'></div>\n" +
+                        "    </body>\n" +
+                        "</html>\n" +
+                        "");
+                }
+            } catch (Exception e){
+                response.sendRedirect("index.html");
             }
         }
     }
